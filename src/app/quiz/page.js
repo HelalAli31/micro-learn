8db26c67f7f9a2676ea3fad6173c23b507a6b898
HelalAli31 <74315954+HelalAli31@users.noreply.github.com>
2025-06-11 14:57:24 +0300

    fix deploy

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,12 +1,6 @@
-// ❌ no "use client"
-export const dynamic = "force-dynamic"; // ✅ prevent static generation
+export const dynamic = "force-dynamic";
+import QuizPageWrapper from "./QuizPageWrapper";
 
-import dynamic from "next/dynamic";
-
-// ✅ Dynamically load component only on client
-const ClientQuizPage = dynamic(() => import("./ClientQuizPage"), {
-  ssr: false, // 👈 this is key
-  loading: () => <div>Loading quiz...</div>,
-});
-
-export default ClientQuizPage; // ✅ no Suspense
+export default function Page() {
+  return <QuizPageWrapper />;
+}
